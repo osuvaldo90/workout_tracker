fea8461ec01da615ba932507396e3f4c65af607e
Osuvaldo Ramos <osuvaldo@towerviewhealth.com>
2019-04-12 21:13:07 -0700

    Blank React project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App (props) {
+  return (
+    <main />
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
